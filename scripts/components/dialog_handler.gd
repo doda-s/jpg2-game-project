d8169b369310d0596cca2f0a7755a3012c54e6ae
doda-s <ph005009004@gmail.com>
2025-12-06 18:06:21 -0300

    refacotr dialog system and add a signal to return player choice mapping

--- scripts/components/dialog_handler.gd
+++ scripts/components/dialog_handler.gd
@@ -1,25 +1,58 @@
 extends Node2D
-class_name DialogHandler
 
-@export_file("*.json") var dialog_json_file: String
-@export var context = ""
+signal dialog_ended_signal(player_choice_mapping: Array[int])
 
-var _dialog_manager: DialogManager
+# The DialogManager is responsible for managing the dialog nodes of a context.
+# This context is received through a DialogHandler.
 
-@onready var _dialog_scoop = {}
+var _current_dialog: String
+var _dialogs: Array
+var _dialog_node_count: int
+var _dialog_view: DialogView
+var _start_in: String
+var _end_in: String
+var _player_choice_mapping: Array[int]
 
-func _ready() -> void:
-	_dialog_manager = get_parent().get_parent().get_node('DialogManager')
+func set_dialog_view(_dialog_view_obj: DialogView):
+	_dialog_view = _dialog_view_obj
 
-func _init_dialog() -> void:
-	_dialog_scoop = _load_dialog_json()
-	for ctx in _dialog_scoop.Context:
-		if ctx.Name == context:
-			_dialog_manager.init_dialog(ctx)
+func init_dialog(ctx: Dictionary) -> void:
+	# Initializes and prepares dialog nodes
+	_current_dialog = ""
+	_dialog_node_count = ctx.DialogNodes.size()
+	_dialogs = ctx.DialogNodes
+	_start_in = ctx.StartIn
+	_end_in = ctx.EndIn
+	_player_choice_mapping = []
+	next_dialog(_start_in)
 
-func _load_dialog_json():
-	if not FileAccess.file_exists(dialog_json_file):
-		print_debug("Dialog file not exist.")
+func next_dialog(_dialog_node_id: String, choice_index: int = -1) -> void:
+	if choice_index >= 0:
+		_player_choice_mapping.append(choice_index)
+	
+	if _current_dialog == _end_in:
+		_debug({}, _player_choice_mapping)
+		_dialog_view.close_dialog()
+		dialog_ended_signal.emit(_player_choice_mapping)
 		return
 	
-	return JSON.parse_string(FileAccess.open(dialog_json_file, FileAccess.READ).get_as_text())
+	for _node in _dialogs:
+		if _node.ID == _dialog_node_id:
+			_debug(_node, [])
+			_dialog_view.show_dialog(_node)
+			_current_dialog = _node.ID
+			break
+
+	_debug({}, _player_choice_mapping)
+
+func _debug(node := {}, player_choice_mapping := []) -> void:
+	
+	if not node.is_empty():
+		print_debug(
+			node.Title,
+			node.Text,
+			node.Options,
+		)
+	
+	if not player_choice_mapping.is_empty():
+		print_debug(player_choice_mapping)
