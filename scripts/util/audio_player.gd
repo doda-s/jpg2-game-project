dbd20002980ce6404f84a4c485ff3b80a1a63c9b
ssumik <joao.sumi@gmail.com>
2025-12-11 16:07:09 -0300

    feat: music added

--- scripts/util/audio_player.gd
+++ scripts/util/audio_player.gd
@@ -1,23 +1,22 @@
 extends AudioStreamPlayer
 
+var music_village = load("res://assets/music/long_tail_blue.mp3")
+
 func set_music_config(music: AudioStream = null, volume: float = 0.5):
 	if music == null:
 		music = get_current_music()
-	
+
 	if stream != music:
 		_set_current_music_and_play(music)
-	
-	if volume_db != linear_to_db(volume):
-		_set_volume(linear_to_db(volume))
-	return
 
-func _set_volume(volume: float):
-	volume_db = volume
+	_set_volume(linear_to_db(volume))
+
+func _set_volume(db_value: float):
+	volume_db = db_value
 
 func _set_current_music_and_play(music: AudioStream):
 	stream = music
 	play()
-	return
 
 func get_current_music():
 	return stream
