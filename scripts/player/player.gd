abde164331e79f836a6e924d7bfe7a55f1f198d5
doda-s <ph005009004@gmail.com>
2025-12-11 16:24:30 -0300

    add player walking animation

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,15 +3,19 @@ class_name Player
 
 var _moviment_component: MovimentComponent
 var _player_controller_component: PlayerControllerComponent
+var _sprite: AnimatedSprite2D
 
 func _ready() -> void:
 	_moviment_component = get_node('MovimentComponent')
 	_player_controller_component = get_node('PlayerControllerComponent')
+	_sprite = get_node("AnimatedSprite2D")
 	
 	if _moviment_component == null:
 		printerr("The player scene needs a MovimentComponent node.")
 	if _player_controller_component == null:
 		printerr("The player scene needs a PlayerControllerComponent node.")
+	if _sprite == null:
+		printerr("The player scene needs a AnimatedSprite2D node.")
 
 func _physics_process(_delta: float) -> void:
 	_player_controller_component.interact()
@@ -28,3 +32,13 @@ func _physics_process(_delta: float) -> void:
 	
 	_moviment_component.moviment_direction(_moviment_direction)
 	_moviment_component.move(_delta) # Contains move_and_slide() 
+
+	if not _moviment_direction.x == 0:
+		_sprite.play("walking")
+	else:
+		_sprite.play("idle")
+
+	if _moviment_direction.x < 0:
+		_sprite.flip_h = true
+	elif _moviment_direction.x > 0:
+		_sprite.flip_h = false
